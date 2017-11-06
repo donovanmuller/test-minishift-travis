9a71576b38b59158f870773605924c63c0594732
Donovan Muller <donovan.muller@gmail.com>
2017-11-06 19:37:25 +0200

    Addresses #6

--- .travis/release.sh
+++ .travis/release.sh
@@ -5,29 +5,29 @@ gem install github_changelog_generator
 
 # Get latest (soon to be previous) release
 previous_release_tag=$(curl -s \
--u ${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN} \
-https://api.github.com/repos/${GITHUB_USERNAME}/test-minishift-travis/releases/latest | \
-    jq -r .tag_name)
+    -u ${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN} \
+    https://api.github.com/repos/${GITHUB_USERNAME}/test-minishift-travis/releases/latest | \
+        jq -r .tag_name)
 
 # Create GitHub release
 release_name="${TRAVIS_TAG:1}"
 github_changelog_generator -t ${GITHUB_ACCESS_TOKEN} -o /tmp/CHANGELOG.md --since-tag ${previous_release_tag}
 release_changelog=$(< /tmp/CHANGELOG.md)
 jq -n \
---arg tag_name "$TRAVIS_TAG" \
---arg release_name "$release_name" \
---arg release_changelog "$release_changelog" \
-'{
-    "tag_name": $tag_name,
-    "name": $release_name,
-    "body": $release_changelog,
-    "draft": false,
-    "prerelease": false
-}' |
-curl -i \
-    -u ${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN} \
-    -d@- \
-    https://api.github.com/repos/${GITHUB_USERNAME}/test-minishift-travis/releases
+    --arg tag_name "$TRAVIS_TAG" \
+    --arg release_name "$release_name" \
+    --arg release_changelog "$release_changelog" \
+    '{
+        "tag_name": $tag_name,
+        "name": $release_name,
+        "body": $release_changelog,
+        "draft": false,
+        "prerelease": false
+    }' |
+    curl -i \
+        -u ${GITHUB_USERNAME}:${GITHUB_ACCESS_TOKEN} \
+        -d@- \
+        https://api.github.com/repos/${GITHUB_USERNAME}/test-minishift-travis/releases
 
 # Generate CHANGELOG.md
 github_changelog_generator -t ${GITHUB_ACCESS_TOKEN}
